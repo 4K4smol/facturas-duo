6f1f373db6e7e6d68766faae22d1a5896cb9ed2d
aka.smol <53124536+4K4smol@users.noreply.github.com>
2026-06-29 22:15:30 +0200

    Añadir notas de diseño vintage

---
+++ facturas-swing/src/main/java/com/facturas/servicio/PdfFacturaVintageNotas.java
@@ -0,0 +1,8 @@
+package com.facturas.servicio;
+
+final class PdfFacturaVintageNotas {
+    static final String NOTA = "Diseño vintage basado en 1-vintage.pdf";
+
+    private PdfFacturaVintageNotas() {
+    }
+}
